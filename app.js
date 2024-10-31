53034328151439111ff2cea037751663578d25cd
CRVanware <crodriguez@vanware.io>
2024-10-31 17:12:38 +0000

    Corrección js 2

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ window.onload = function() {
   let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
   let action = ["ate ", "peed ", "crushed ", "broke "];
   let what = ["my homework ", "my phone ", "the car "];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying",
-  ];
+  let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch","while I was praying"];
   let var1 = Math.floor(Math.random() * (who.length - 1));
   let var2 = Math.floor(Math.random() * (action.length - 1));
   let var3 = Math.floor(Math.random() * (what.length - 1 ));
